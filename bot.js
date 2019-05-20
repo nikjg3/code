d03d734f72068ed176f9cd126523dac663fe3802
Nikolas <Nikolas@192.168.0.5>
2019-05-21 00:30:32 +1000

    Created basic functions and setup commands

--- bot.js
+++ bot.js
@@ -1,21 +1,106 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const TOKEN = 'NTc3NDUzMjc3Mzg2NjM3MzEz.XNlSUg.SUPK9tCJj7DwBns0gBNY6VFQL7w';
+const { prefix, token } = require('./config.json');
 
+//Created variables
+var que = []; // Stores players waiting in que
+var players = [];// Stores players in the game
+var mainChannel;
+var generalChannel = "575496493360873500"
+
+
+// Finds messages that start with the prefix to be excuted as commands (see function processCommand)
+client.on('message', (receivedMessage) => {
+    if (receivedMessage.author == client.user) {
+        return
+    }
+    
+    if (receivedMessage.content.startsWith(prefix)) {
+        processCommand(receivedMessage)
+    }
+})
+
+// Confirms login in the console
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('I dont do drugs');
-  }
-});
+// Lists all players in the que
+function listPlayers(arguments, receivedMessage)
+{
+	if (mainChannel != null) {
+			mainChannel.send("Players currently in que")
+			mainChannel.send(que)
+	}
+	else {
+		receivedMessage.channel.send("Please use `" + prefix + "setup` to set a channel");
+	}
 
-client.on('message', msg => {
-  if (msg.content === 'pong') {
-    message.member.send('I just want it all to end');
-  }
-});
-client.login(TOKEN);
+}
+
+//Adds the player to the que
+function joinGame(arguments, receivedMessage)
+{
+	if (mainChannel != null){
+		mainChannel.send(receivedMessage.member.user + " has joined the game");
+		que.push(receivedMessage.member.user) 
+	}	
+	else {
+		receivedMessage.channel.send("Please use `" + prefix + "setup` to set a channel");
+	}
+}
+
+//Defines what channel messages will be sent in
+function setup(arguments, receivedMessage)
+{
+	mainChannel = receivedMessage.channel
+	mainChannel.send("The bot will now use this channel");
+}
+
+//A basic help function
+function help(arguments, receivedMessage)
+{
+	if (mainChannel != null) {
+		mainChannel.send("Commands: " + prefix + "help " + prefix + "join " + prefix + "setup ");
+	}
+	else {
+		receivedMessage.channel.send("Please use `" + prefix + "setup` to set a channel");
+	}
+}
+
+//Starts the game
+function startGame(arguments, receivedMessage)
+{
+	players = que; //Moves the que to the game
+}
+
+//Processes commands by sepearting
+function processCommand(receivedMessage) {
+	let fullCommand = receivedMessage.content.substr(1)
+    let splitCommand = fullCommand.split(" ")
+    let primaryCommand = splitCommand[0]
+    let arguments = splitCommand.slice(1)
+
+    console.log("Command received: " + primaryCommand)
+    console.log("Arguments: " + arguments) 
+
+    if (primaryCommand == "help") {
+        help(arguments, receivedMessage)
+    } 
+	else if (primaryCommand == "setup") {
+        setup(arguments, receivedMessage)
+	}
+	else if (primaryCommand == "join") {
+        joinGame(arguments, receivedMessage)
+	}
+	else if (primaryCommand == "players") {
+        listPlayers(arguments, receivedMessage)
+	}
+	else {
+        receivedMessage.channel.send("I don't understand the command. Use please use " + prefix + "help")
+    }
+}
+
+
+client.login(token);
