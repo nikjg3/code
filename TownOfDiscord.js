fbac913536f2626cd308b8d4939897e7be3b8933
CameronvanRoon <s3382032@student.rmit.edu.au>
2019-05-20 23:05:50 +1000

    Tutorial progress 1

--- TownOfDiscord.js
+++ TownOfDiscord.js
@@ -12,6 +12,11 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+const args = message.content.slice(prefix.length).split(/ +/);
+const command = args.shift().toLowerCase();
+
     if (message.content === `${prefix}ping`) {
         message.channel.send('Don\'t do drugs');
     } else if (message.content === `${prefix}beep`) {
@@ -19,6 +24,19 @@ client.on('message', message => {
     }
     else if (message.content === `${prefix}server`) {
         message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+    }
+    else if (message.content === `${prefix}DM`) {
+        message.author.send(`Slide right in`);
+    }
+    else if (command === 'args-info') {
+        if (!args.length) {
+            return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+        }
+    	else if (args[0] === 'foo') {
+            return message.channel.send('bar');
+        }
+    
+        message.channel.send(`First argument: ${args[0]}`);
     }
 });
 
