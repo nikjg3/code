930ce7169749bd1c1be671af5d53eb45b745b318
nikjg3 <nikolasgrubits@hotmail.com>
2020-09-23 09:47:52 +1000

    fix

--- TownOfDiscord.js
+++ TownOfDiscord.js
@@ -1,8 +1,10 @@
 // require the discord.js module
 const Discord = require('discord.js');
+const fs = require('fs');
 
 var nigger = ["nig","nigger","niger","nigg"];
-var nigcount = 0;
+
+var nigger2 = /(?:^|\W)nig(?:$|\W)|(?:^|\W)nigger(?:$|\W);
 
 const { prefix, token } = require('./config.json');
 // create a new Discord client
@@ -14,10 +16,19 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 client.on('message', message => {
+const nigcount = require('./nig.json'); 
+const ID = message.author.id;
+console.log(nigcount[ID]);
+if(nigcount[ID] == null)
+{
+	nigcount[ID] = 0;
+	fs.writeFileSync('./nig.json', JSON.stringify(nigcount));
+}
 for (var i = 0; i < nigger.length; i++) {
-  if (message.content.includes(nigger[i])) {
-	nigcount = nigcount + 1;
-    message.channel.send('Thats ' + nigcount + " nigs!");
+  if (message.content.match(nigger2)) {
+	nigcount[ID] += 1;
+	fs.writeFileSync('./nig.json', JSON.stringify(nigcount));
+    message.channel.send(message.author.username + ' has been a naughty boy ' + nigcount[ID] + " times!");
 	message.pin({ reason: 'nigger' })
     break;
   }
